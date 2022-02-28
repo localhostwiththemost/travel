3df2aec99929ede102546773a266b6da8ee93a4b
localhostwiththemost <jpowelldev@gmail.com>
2022-02-28 12:27:41 -0600

    Adjusted footer social link

--- index.php
+++ index.php
@@ -65,6 +65,7 @@ $message_sent = true;
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet" />
+    <!--FAVICON-->
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
